18150450b9eb9aaffed23115920287a435344f91
Ilya Kazanevich <93613922+Ilyakaz02@users.noreply.github.com>
2022-03-23 12:19:59 +0300

    Initial commit

---
+++ history.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# *** Spyder Python Console History Log ***
+
+## ---(Mon Nov  8 11:32:25 2021)---
+test_list_tour = [1, 2, 3, 4, 5]
+test_dict_tour = {'a': 1, 'b': 2}
+runfile('C:/Users/425/.spyder-py3/temp.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
+
+## ---(Mon Nov  8 20:44:19 2021)---
+runfile('C:/Users/425/.spyder-py3/temp.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
+runcell(0, 'C:/Users/425/.spyder-py3/temp.py')
+runfile('C:/Users/425/.spyder-py3/temp.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
+runfile('C:/Users/425/.spyder-py3/untitled0.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
+runfile('C:/Users/425/.spyder-py3/untitled0.py', wdir='C:/Users/425/.spyder-py3')
+runfile('C:/Users/425/.spyder-py3/untitled0.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
+runfile('C:/Users/425/.spyder-py3/untitled0.py', wdir='C:/Users/425/.spyder-py3')
+
+## ---(Wed Nov 10 17:03:38 2021)---
+runfile('C:/Users/425/.spyder-py3/temp.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
+runcell(0, 'C:/Users/425/.spyder-py3/temp.py')
+runfile('C:/Users/425/.spyder-py3/temp.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
+runcell(0, 'C:/Users/425/.spyder-py3/temp.py')
+
+## ---(Fri Nov 12 22:07:14 2021)---
+runfile('C:/Users/425/.spyder-py3/temp.py', wdir='C:/Users/425/.spyder-py3', post_mortem=True, current_namespace=True)
